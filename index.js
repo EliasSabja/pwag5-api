30c59d3e7c32c7453acc0d944e7bd91af68d6b18
elias <elias.sabja@uc.cl>
2022-05-07 18:07:28 -0400

    From typescript to not typescript

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+const express = require("express");
+const dotenv = require("dotenv");
 const cors = require('cors');
 const webpush = require('web-push');
-dotenv_1.default.config();
-const app = (0, express_1.default)();
-const router = express_1.default.Router();
+
+dotenv.config();
+
+const app = express();
+const router = express.Router();
 const port = process.env.PORT || 3000;
+
 app.use(cors());
+
 const vapidKeys = webpush.generateVAPIDKeys();
+
 webpush.setGCMAPIKey(process.env.APPLICATION_SERVER_PUBLIC_KEY);
-webpush.setVapidDetails('mailto:${process.env.MAIL}', vapidKeys.publicKey, vapidKeys.privateKey);
+webpush.setVapidDetails(
+  'mailto:${process.env.MAIL}',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
+
 app.get('/', (req, res) => {
-    console.log(vapidKeys);
-    res.send('Express + TypeScript Server full speed');
+  console.log(vapidKeys);
+  res.send('Express + TypeScript Server full speed');
 });
+
 app.get('/subscribe', (req, res) => {
-    const subscription = req.body;
-    res.status(201).json({});
-    const payload = JSON.stringify({ title: 'Section.io Push notification' });
-    webpush.sendNotification(subscription, payload).catch((err) => console.log(err));
+  
+  const subscription = req.body;
+
+  res.status(201).json({});
+
+  const payload = JSON.stringify({title: 'Section.io Push notification'});
+
+  webpush.sendNotification(subscription, payload).catch((err) => console.log(err));
 });
+
+
 app.listen(port, () => {
-    console.log(`[server]: Server is running full speed at https://localhost:${port}`);
+  console.log(`[server]: Server is running full speed at https://localhost:${port}`);
 });
